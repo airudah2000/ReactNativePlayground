8f8c9085188da98f33cff0ab4915e64316a480b8
rob <robert.a.udah@bath.edu>
2018-12-27 13:01:47 +0000

    New backup dir for icons milestone. Extended app to include a menu header with a logo, adding comments for menu items and functionality for selecting a favourite meal.

--- components/NavigationIcons/MainComponent.js
+++ components/NavigationIcons/MainComponent.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import {View, Platform, Image, StyleSheet, ScrollView, Text} from 'react-native';
+import {View, Platform} from 'react-native';
 import Menu from './MenuComponent'
 import Home from './HomeComponent'
 import Contact from './ContactComponent'
 import About from './AboutComponent'
 import DishDetail from './DishDetailComponent';
-import {createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView} from 'react-navigation';
+import {createStackNavigator, createDrawerNavigator} from 'react-navigation';
 import {Icon} from 'react-native-elements';
 
 
@@ -95,22 +95,6 @@ const HomeNavigator = createStackNavigator({
     })
 });
 
-const CustomDrawerContentComponent = (props) => (
-    <ScrollView>
-        <SafeAreaView style={styles.container} forceInset={{top: 'always', horizontal: 'never'}}>
-            <View style={styles.drawerHeader}>
-                <View style={{flex: 1}}>
-                    <Image source={require('./images/logo.png')} style={styles.drawerImage} />
-                </View>
-                <View style={{flex: 2}}>
-                    <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
-                </View>
-            </View>
-            <DrawerItems {...props} />
-        </SafeAreaView>
-    </ScrollView>
-);
-
 const MainNavigator = createDrawerNavigator({
     Home: {
         screen: HomeNavigator,
@@ -169,8 +153,7 @@ const MainNavigator = createDrawerNavigator({
         },
     }
 }, {
-    drawerBackgroundColor: '#D1C4E9',
-    contentComponent: CustomDrawerContentComponent
+    drawerBackgroundColor: '#D1C4E9'
 });
 
 class Main extends Component {
@@ -183,29 +166,5 @@ class Main extends Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    drawerHeader: {
-        backgroundColor: '#512DA8',
-        height: 140,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        flexDirection: 'row'
-    },
-    drawerHeaderText: {
-        color: 'white',
-        fontSize: 24,
-        fontWeight: 'bold'
-    },
-    drawerImage: {
-        margin: 10,
-        width: 80,
-        height: 60
-    }
-});
 
 export default Main
