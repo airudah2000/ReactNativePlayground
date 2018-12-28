b4f2e82becf970bb6f23a4226a0a83f422da7ac9
rob <robert.a.udah@bath.edu>
2018-12-28 12:49:49 +0000

    Implemented ActivityMonitor (Loading message) Downgraded react-native-vector-icons to get rid of npm warning messages

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import {FlatList, ScrollView} from 'react-native';
 import {Card, ListItem, Text} from 'react-native-elements';
 import {connect} from 'react-redux';
 import {baseUrl} from "../shared/baseUrl";
+import {Loading} from "./LoadingComponent";
 
 const mapStateToProps = state => {
     return {
@@ -29,34 +30,52 @@ class About extends Component {
             )
         };
 
-        return (
-            <ScrollView>
-                <Card title={"Our History"}>
-                    <Text style={{marginBottom: 10}}>
-                        Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par
-                        excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found
-                        nowhere else, it enjoys patronage from the A-list clientele in Hong Kong. Featuring four
-                        of
-                        the best three-star Michelin chefs in the world, you never know what will arrive on your
-                        plate the next time you visit us.
-                    </Text>
-                    <Text style={{marginBottom: 10}}>
-                        The restaurant traces its humble beginnings to The Frying Pan, a successful chain
-                        started by
-                        our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a
-                        pan.
-                    </Text>
-                </Card>
-                <Card title={"Corporate Leadership"}>
-                    <FlatList
-                        data={this.props.leaders.leaders}
-                        renderItem={renderListItems}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
-            </ScrollView>
-        )
+        if(this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <Card title={"Corporate Leadership"}>
+                        <Loading/>
+                    </Card>
+                </ScrollView>
+            );
+        } else if(this.props.errMess){
+            return(
+                <ScrollView>
+                    <Card title={"Corporate Leadership"}>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        } else {
+            return (
+                <ScrollView>
+                    <Card title={"Our History"}>
+                        <Text style={{marginBottom: 10}}>
+                            Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par
+                            excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found
+                            nowhere else, it enjoys patronage from the A-list clientele in Hong Kong. Featuring four
+                            of
+                            the best three-star Michelin chefs in the world, you never know what will arrive on your
+                            plate the next time you visit us.
+                        </Text>
+                        <Text style={{marginBottom: 10}}>
+                            The restaurant traces its humble beginnings to The Frying Pan, a successful chain
+                            started by
+                            our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a
+                            pan.
+                        </Text>
+                    </Card>
+                    <Card title={"Corporate Leadership"}>
+                        <FlatList
+                            data={this.props.leaders.leaders}
+                            renderItem={renderListItems}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
+                </ScrollView>
+            )
+        }
     }
 }
 
-export default connect(mapStateToProps) (About);
+export default connect(mapStateToProps)(About);
